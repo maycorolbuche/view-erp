3d00e80bbfcc4faf1679d32c5934915e426b8cca
maycorolbuche <mayco_rolbuche@hotmail.com>
2023-10-18 19:52:04 -0300

    Migration

---
+++ database/migrations/2023_10_18_225117_create_users_systems.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersSystems extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users_systems', function (Blueprint $table) {
+            $table->increments('id_user_system');
+            $table->unsignedInteger('id_user')->nullable();
+            $table->unsignedInteger('id_system')->nullable();
+
+            $table->foreign('id_user')->references('id_user')->on('users');
+            $table->foreign('id_system')->references('id_system')->on('systems');
+
+            $table->unique(['id_user', 'id_system']);
+
+            $table->unsignedInteger('created_by')->nullable();
+            $table->unsignedInteger('updated_by')->nullable();
+            $table->timestamps();
+
+            $table->foreign('created_by')->references('id_user')->on('users');
+            $table->foreign('updated_by')->references('id_user')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users_systems');
+    }
+}
