c60fbb316afb5ffd17ed4210444f8c701a0d7893
maycorolbuche <mayco_rolbuche@hotmail.com>
2026-05-11 19:24:39 -0300

    Ajustes horários jobs

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -19,20 +19,20 @@ class Kernel extends ConsoleKernel
         // php artisan schedule:run
         Configs::set("cron.run.start", now());
 
-        $schedule->command('task:close_authorizations')->daily();
-        $schedule->command('task:close_batches_without_refund')->daily();
+        $schedule->command('task:close_authorizations')->dailyAt('00:00');
+        $schedule->command('task:close_batches_without_refund')->dailyAt('00:00');
 
-        $schedule->command('task:remove_inactive_expenses')->daily();
-        $schedule->command('task:email_pending_batches')->daily();
+        $schedule->command('task:remove_inactive_expenses')->dailyAt('00:30');
+        $schedule->command('task:email_pending_batches')->dailyAt('00:30');
 
-        $schedule->command('task:migrate')->daily();
-        $schedule->command('task:seed')->daily();
-        $schedule->command('task:remove_old_logs')->daily();
+        $schedule->command('task:migrate')->dailyAt('03:00');
+        $schedule->command('task:seed')->dailyAt('03:00');
+        $schedule->command('task:remove_old_logs')->dailyAt('04:00');
 
-        $schedule->command('task:batch_approved_when_paid')->daily();
-        $schedule->command('task:delete_excess_files')->daily();
+        $schedule->command('task:batch_approved_when_paid')->dailyAt('01:00');
+        $schedule->command('task:delete_excess_files')->dailyAt('01:30');
 
-        $schedule->command('task:backup')->daily();
+        $schedule->command('task:backup')->dailyAt('23:00');
 
         Configs::set("cron.run.end", now());
 
